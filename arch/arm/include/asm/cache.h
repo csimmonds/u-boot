2d454a139f15eaca7dd1a840e6b939a9b05a99ae
SRICHARAN R <r.sricharan@ti.com>
2013-01-08 08:08:23 +0000

    ARM: mmu: Introduce weak dram_bank_setup function
    Introduce a weak version of dram_bank_setup function
    to allow a platform specific function.
    
    This is used in the subsequent patch to setup dram region
    without 'XN' attribute in order to enable the region
    under client permissions.
    
    Signed-off-by: R Sricharan <r.sricharan@ti.com>
    Cc: Vincent Stehle <v-stehle@ti.com>
    Cc: Tom Rini <trini@ti.com>

--- arch/arm/include/asm/cache.h
+++ arch/arm/include/asm/cache.h
@@ -42,6 +42,7 @@ static inline void invalidate_l2_cache(void)
 void l2_cache_enable(void);
 void l2_cache_disable(void);
 
+void dram_bank_mmu_setup(int bank);
 /*
  * The current upper bound for ARM L1 data cache line sizes is 64 bytes.  We
  * use that value for aligning DMA buffers unless the board config has specified
