4065722c28ef814d3072fb8456e49c0b0a387d12
Lokesh Vutla <lokeshvutla@ti.com>
2013-02-12 21:29:09 +0000

    arm: dra7xx: Add dra7xx_evm build support
    Adding the build support for dra7xx_evm.
    Reusing omap5_evm.h config by moving it to omap5_common.h
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    Signed-off-by: R Sricharan <r.sricharan@ti.com>

--- include/configs/dra7xx_evm.h
+++ include/configs/dra7xx_evm.h
@@ -1,9 +1,9 @@
 /*
  * (C) Copyright 2013
  * Texas Instruments Incorporated.
- * Sricharan R	  <r.sricharan@ti.com>
+ * Lokesh Vutla	  <lokeshvutla@ti.com>
  *
- * Configuration settings for the TI EVM5430 board.
+ * Configuration settings for the TI DRA7XX board.
  * See omap5_common.h for omap5 common settings.
  *
  * See file CREDITS for list of people who contributed to this
@@ -25,19 +25,15 @@
  * MA 02111-1307 USA
  */
 
-#ifndef __CONFIG_OMAP5_EVM_H
-#define __CONFIG_OMAP5_EVM_H
+#ifndef __CONFIG_DRA7XX_EVM_H
+#define __CONFIG_DRA7XX_EVM_H
 
 #include <configs/omap5_common.h>
 
-#define CONFIG_OMAP5430	/* which is in a 5430 */
-#define CONFIG_5430EVM	/* working with EVM */
+#define CONFIG_OMAP
+#define CONFIG_DRA7XX		/* in a TI DRA7XX core */
+#define CONFIG_DRA752_EVM
 
-/* TWL6035 */
-#ifndef CONFIG_SPL_BUILD
-#define CONFIG_TWL6035_POWER
-#endif
+#define CONFIG_SYS_PROMPT		"DRA752 EVM # "
 
-#define CONFIG_SYS_PROMPT		"OMAP5430 EVM # "
-
-#endif /* __CONFIG_OMAP5_EVM_H */
+#endif /* __CONFIG_DRA7XX_EVM_H */
