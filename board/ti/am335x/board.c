9ebd000a87d3613d94a4fda021e763d9803cc72a
hvaibhav@ti.com <hvaibhav@ti.com>
2013-03-14 21:11:16 +0000

    am335x: Enable DDR PHY dynamic power down bit for DDR3 boards
    Enable DDR PHY dynamic power down bit, which enables
    powering down the IO receiver when not performing read.
    
    This also helps in reducing overall power consumption in
    low power states (suspend/standby).
    
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>
    Signed-off-by: Satyanarayana, Sandhya <sandhya.satyanarayana@ti.com>
    Cc: Tom Rini <trini@ti.com>
    Reviewed-by: Tom Rini <trini@ti.com>

--- board/ti/am335x/board.c
+++ board/ti/am335x/board.c
@@ -346,7 +346,8 @@ static struct emif_regs ddr3_emif_reg_data = {
 	.sdram_tim2 = MT41J128MJT125_EMIF_TIM2,
 	.sdram_tim3 = MT41J128MJT125_EMIF_TIM3,
 	.zq_config = MT41J128MJT125_ZQ_CFG,
-	.emif_ddr_phy_ctlr_1 = MT41J128MJT125_EMIF_READ_LATENCY,
+	.emif_ddr_phy_ctlr_1 = MT41J128MJT125_EMIF_READ_LATENCY |
+				PHY_EN_DYN_PWRDN,
 };
 
 void am33xx_spl_board_init(void)
